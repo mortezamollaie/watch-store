126760c668e00f39889b789266f847ebb13aa980
mortezamollaie <mm.gov.1381@gmail.com>
2024-07-19 23:59:56 +0330

    send sms api

---
+++ app/Models/SmsCode.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SmsCode extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'mobile',
+        'code',
+    ];
+
+    public static function checkTwoMinute($mobile){
+        $check = self::query()->where('mobile', $mobile)
+            ->where('created_at', '>', Carbon::now()->subMinutes(2))->first();
+        if($check){
+            return true;
+        }
+        return false;
+    }
+
+    public static function createSmsCode($mobile, $code){
+        self::query()->create([
+            'mobile' => $mobile,
+            'code' => $code,
+        ]);
+    }
+}
